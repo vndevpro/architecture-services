15906210e1f7b4eb0a88fc5e325396b65ff82954
juanonsoftware <juanonsoftware@github.com>
2017-07-19 11:57:34 +0700

    Implement getDetailedById method

--- GdNet.Layers.AppCommon.ServicesImpl/AttachmentService.cs
+++ GdNet.Layers.AppCommon.ServicesImpl/AttachmentService.cs
@@ -8,19 +8,24 @@ namespace GdNet.Layers.AppCommon.ServicesImpl
 {
     public class AttachmentService : ServiceBase<AttachmentDto, Attachment>, IAttachmentService
     {
+        private readonly IAttachmentRepository _repository;
+
         public AttachmentService(IAttachmentRepository repository, IMapperProvider mapperProvider)
             : base(repository, mapperProvider)
         {
+            _repository = repository;
         }
 
         public AttachmentService(IAttachmentRepository repository, IMapperProvider mapperProvider, IEntitySavingStrategy<AttachmentDto, Attachment> savingStrategy)
             : base(repository, mapperProvider, savingStrategy)
         {
+            _repository = repository;
         }
 
         public DetailAttachmentDto GetDetailById(Guid id)
         {
-            throw new NotImplementedException();
+            var attachment = _repository.GetById(id);
+            return MapperProvider.Map(attachment, new DetailAttachmentDto());
         }
     }
 }
