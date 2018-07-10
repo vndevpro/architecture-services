880ea92154202d2139f7099fc651bce2b2852508
juanonsoftware <juanonsoftware@github.com>
2018-07-10 18:26:11 +0700

    Include Note field into AttachmentDto object

--- GdNet.Layers.AppCommon.ServicesImpl/Mappers/AttachmentDtoMapperFactory.cs
+++ GdNet.Layers.AppCommon.ServicesImpl/Mappers/AttachmentDtoMapperFactory.cs
@@ -6,6 +6,12 @@ namespace GdNet.Layers.AppCommon.ServicesImpl.Mappers
 {
     public class AttachmentDtoMapperFactory : DefaultEditableEntityDtoMapperFactory<Attachment, AttachmentDto>
     {
-        
+        protected override void OnMapperCreated()
+        {
+            base.OnMapperCreated();
+
+            MappingExpression
+                .ForMember(p => p.Note, a => a.MapFrom(p => p.Note));
+        }
     }
 }
